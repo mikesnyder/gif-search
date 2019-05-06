bd4bb1cdf401c1b8eaf1b0dda0a76712234ae394
Mike Snyder <mikesnyder88@gmail.com>
2019-05-06 16:57:00 -0500

    animate and pause function working

--- script.js
+++ script.js
@@ -1,20 +1,4 @@
 
-//Gif animate, pause, start function
-// $(".gif").on("click", function () {
-
-//     let animateUrl = $(this).attr("data-animate");
-//     let stillUrl = $(this).attr("data-still");
-//     let state = $(this).attr("data-state");
-
-//     if (state === "still") {
-//         $(this).attr("src", animateUrl),
-//             $(this).attr("data-state", "animating")
-//     }
-//     else {
-//         $(this).attr("src", stillUrl),
-//             $(this).attr("data-state", "still")
-//     };
-// });
 
 //Starting Gif Category buttons
 var categories = ["Dogs", "Chris Farley", "Star Wars", "Sloth"];
@@ -27,55 +11,81 @@ function displayGifs() {
     var apiKey = "um450KSCPU0N2DvYWIYBhflCy4GX34Mr"
 
     //Number of gifs returned
-    var qty = "1"
-    var queryURL =
-        "https://api.giphy.com/v1/gifs/search?q=" + category + "&api_key=" + apiKey + "&limit=" + qty;
-    // Creates AJAX call for the specific movie button being clicked
-    console.log(queryURL)
+    var qty = 1;
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + category + "&api_key=" + apiKey + "&limit=" + qty;
+    var i = 0;
+    // Creates AJAX call for the specific category button being clicked
+    
+   
+
 
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-    console.log(response.data[0].rating)
-        // Creates a div to hold the movie
+        // Creates a div to hold the category
         let wrapper = $("<div>");
         // Retrieves the Rating Data
-        let i = 0;
+       
         let rating = response.data[i].rating;
         // Creates an element to have the rating displayed
         let ratingElement = $("<p>");
         // Displays the rating
         ratingElement.text(rating);
-        wrapper.append(ratingElement);
+        wrapper.prepend(ratingElement);
 
     
         
         // Creates an element to hold the image
-        let posterElement = $("<img>");
-        let posterUrl = response.data[i].images.fixed_height_still.url;
+        var gifElement = $("<img>");
+        var gifUrl = response.data[i].images.fixed_height_still.url;
+        var gifAnimate = response.data[i].images.fixed_height.url;
+        var gifStill = response.data[i].images.fixed_height_still.url;
         // Appends the image
-        posterElement.attr('src', posterUrl)
-        wrapper.append(posterElement);
-        // Puts the entire Movie above the previous movies.
+        gifElement.attr('src', gifUrl);
+        gifElement.attr('data-still', gifStill);
+        gifElement.attr('data-animate', gifAnimate);
+        gifElement.attr('data-state', 'still');
+        gifElement.attr('class', 'gif');
+        wrapper.append(gifElement);
+        // Puts the gif above other gifs
         $("#category-view").prepend(wrapper);
+
+
+        $(".gif").on("click", function () {
+
+            let animateUrl = $(this).attr("data-animate");
+            let stillUrl = $(this).attr("data-still");
+            let state = $(this).attr("data-state");
+        
+            if (state == "still") {
+                $(this).attr("src", animateUrl),
+                    $(this).attr("data-state", "animating")
+            }
+            else {
+                $(this).attr("src", stillUrl),
+                    $(this).attr("data-state", "still")
+            };
+        });
     });
+
+
 
 }
 
-// Function for displaying movie data
+// Function for displaying category data
 function renderButtons() {
 
-    // Deletes the movies prior to adding new movies
+    // Deletes the gif buttons prior to adding new buttons
     // (this is necessary otherwise you will have repeat buttons)
     $("#gif-buttons").empty();
-    // Loops through the array of movies
+    // Loops through the array of categories
     for (var i = 0; i < categories.length; i++) {
 
-        // Then dynamicaly generates buttons for each movie in the array
+        // Then dynamicaly generates buttons for each category in the array
         // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
         var a = $("<button>");
-        // Adds a class of movie to our button
+        // Adds a class of 'category' to our button
         a.addClass("category");
         // Added a data-attribute
         a.attr("data-name", categories[i]);
@@ -85,22 +95,23 @@ function renderButtons() {
         $("#gif-buttons").append(a);
     }
 }
-//Step-1
-// This function handles events where the add movie button is clicked
+
 $("#add-category").on("click", function(event) {
     event.preventDefault();
     // This line of code will grab the input from the textbox
     var categoryAdd = $("#category-input").val().trim();
 
-    // The movie from the textbox is then added to our array
+    // The input from text box is pushed  
     categories.push(categoryAdd);
-    console.log(categories)
-    // Calling renderButtons which handles the processing of our movie array
+    // Calling renderButtons which handles the processing of our category array
     renderButtons();
 });
 
-// Adding click event listeners to all elements with a class of "movie"
+// Adding click event listeners to all elements with a class of "category"
 $(document).on("click", ".category", displayGifs);
 
 // Calling the renderButtons function to display the intial buttons
 renderButtons();
+
+
+//Gif animate, pause, start function
